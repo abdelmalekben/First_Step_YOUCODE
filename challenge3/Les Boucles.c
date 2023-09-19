1da8a952cdf557c3910d8018ed5f0cc9f92f665f
[hello world] <[b.benabdelmalek94@gmail.com]>
2023-09-19 09:26:36 +0100

    les boucles

---
+++ challenge3/Les Boucles.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	   int N;
+	   do {
+	   printf("Mot de pass:");
+	   scanf("%d",&N); 
+	   } 
+	   for (N=0;N<=3;N++)	{
+	   
+	   	 printf("eror");}
+	   while (N!=123);{
+	   	 printf("bienvenu!");
+	   
+	  }
+	  
+	  
+	 return 0 ;
+	  }
+	  
+	  
+	  
+	  
+	 
+	
+	
+	
